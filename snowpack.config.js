f29a8f9d7b84f10ab49e98cd39e34785e3d94bcb
Kyle <kyle@healthspaces.com>
2021-03-06 16:37:59 -0700

    updated readme and cleaned up snowpack config

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,30 +1,6 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
-  alias: {
-    "react": "preact/compat",
-    "react-dom": "preact/compat"
-  },
   mount: {
     "src": "/"
-  },
-  plugins: [
-    /* ... */
-  ],
-  routes: [
-    /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
-  ],
-  optimize: {
-    /* Example: Bundle your final build: */
-    // "bundle": true,
-  },
-  packageOptions: {
-    /* ... */
-  },
-  devOptions: {
-    /* ... */
-  },
-  buildOptions: {
-    /* ... */
   },
 };
